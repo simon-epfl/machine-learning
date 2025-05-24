f6a27a2eb276409611503e6611591f8bf831751c
Androz2091 <androz2091@gmail.com>
2025-05-24 11:23:30 +0200

    update embdings

--- embeddings.py
+++ embeddings.py
@@ -3,7 +3,7 @@ import gensim.downloader as api
 model = api.load("glove-wiki-gigaword-50")
 
 # pizza - italy = france - ?
-result = model.most_similar(positive=["pizza", "tunisia"], negative=["italy"], topn=5)
+result = model.most_similar(positive=["pizza", "korea"], negative=["italy"], topn=5)
 
 for word, score in result:
     print(f"{word}: {score:.4f}")
