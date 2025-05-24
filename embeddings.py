4637b5c9104a5fb9aed00a9db1a8c17b2a25f5b3
Androz2091 <androz2091@gmail.com>
2025-05-24 11:17:31 +0200

    work on embeddings

---
+++ embeddings.py
@@ -0,0 +1,12 @@
+import gensim.downloader as api
+
+model = api.load("glove-wiki-gigaword-50")
+
+# pizza - italy = france - ?
+result = model.most_similar(positive=["pizza", "tunisia"], negative=["italy"], topn=5)
+
+for word, score in result:
+    print(f"{word}: {score:.4f}")
+
+vector = model["carthage"]
+print(vector)
